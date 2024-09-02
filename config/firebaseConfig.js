20ff301a1ab9ba6f3691a8259f577925a9e8ef63
sutariyahasti <hastisutariya01@gmail.com>
2024-09-02 18:42:37 +0530

    firebase creadinatial unloaded

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,9 +1,22 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../admin-penal-97654-firebase-adminsdk-z9ix8-cb954a6122.json'); // Replace with your Firebase service account key file path
+// const { serviceAccount } = require('../admin-penal-97654-firebase-adminsdk-z9ix8-cb954a6122');
 
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // Handle newlines correctly
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+  };
+  
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'admin-penal-97654.appspot.com' // Replace with your Firebase Storage bucket name
+  storageBucket: 'admin-penal-97654.appspot.com' 
 });
 
 const bucket = admin.storage().bucket();
